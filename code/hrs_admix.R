712bcc6b248a6a18f0ef32a4bc56ae096b056e45
Alex Diaz-Papkovich <alex.diaz-papkovich@mail.mcgill.ca>
2019-01-11 14:16:46 -0500

    Add regression model and plotting

--- code/hrs_admix.R
+++ code/hrs_admix.R
@@ -1,4 +1,11 @@
+library(ggplot2)
+
 # Run some linear regression on admixture levels on the HRS
+
+#Required results:
+# 1. Summary of model Native American ancestry by birth year
+# 2. Scatterplot of Native American ancestry as a function of birth year in the Mexican Americans with the above trend line from the model
+# 3. Range of Native American ancestry in the Mexican Americans in HRS. (my note: average by birth year?)
 
 data_dir <- "/Volumes/Stockage/alex/hrs/aux"
 data_file <- "mexam.csv"
@@ -13,8 +20,13 @@ hrs_data_mex$BirthRegionNum <- as.factor(hrs_data_mex$BirthRegionNum)
 # We're interested in the three admixture values
 # ADMIX1 = AFR
 # ADMIX2 = EUR
-# ADMIX3 = Native and/or Asian
+# ADMIX3 = Native American / Asian
 
 lm_admix <- lm(ADMIX3 ~ BirthYear, data = hrs_data_mex)
 summary(lm_admix)
 
+ggplot(data = hrs_data_mex, aes(y = ADMIX3, x = BirthYear)) + 
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  xlab("Birth Year") + ylab("Estimated admixture level") +
+  ggtitle("Estimated admixture level vs birth year (Self-identified Mexican-Americans, HRS)")

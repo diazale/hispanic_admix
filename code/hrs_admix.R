7d6c1e363968762b464d0da9e2817e72a42488c1
Alex Diaz-Papkovich <alex.diaz-papkovich@mail.mcgill.ca>
2019-01-22 14:30:05 -0500

    remove some influential observations and trim to self-ID white popn

--- code/hrs_admix.R
+++ code/hrs_admix.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(boot)
+library(ggridges)
 
 # Run some linear regression on admixture levels on the HRS
 
@@ -37,6 +39,11 @@ summary(lm_admix_eur)
 lm_admix_afr <- lm(ADMIX1 ~ BirthYear, data = hrs_data_mex)
 summary(lm_admix_afr)
 
+# Drop the 4 most recent births (only 4 born 1970+)
+hrs_data_mex_temp <- subset(hrs_data_mex, BirthYear < 1970 && as.character(Race_HRS) == "White")
+lm_admix_trimmed <- lm(ADMIX3 ~ BirthYear, data = hrs_data_mex_temp)
+summary(lm_admix_trimmed)
+
 ggplot(data = hrs_data_mex, aes(y = ADMIX3, x = BirthYear)) + 
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
@@ -73,3 +80,71 @@ ggplot(mean_admix, aes(ADMIX3)) +
   xlab("Estimated admixture level") +
   ggtitle("Densities of estimated Native American admixture by age range (self-identified Mexican-Americans, HRS)") +
   ggsave(paste(img_dir, "age_range_density.jpeg", sep = "/"))
+
+##### Do 1000 bootstrap replicates of Mex-Am Native ancestry by decade and make a ridgeline plot #####
+# Can you do 1000 bootstrap sampling iterations for Native American ancestry for each decade of the
+# HRS Mexican Americans and then plot the distribution of means in a ridgeline plot?
+
+# Define a decade variable
+min(hrs_data_mex$BirthYear)
+max(hrs_data_mex$BirthYear)
+hrs_data_mex$BirthDecade[hrs_data_mex$BirthYear < 1920] <- "1910-19"
+hrs_data_mex$BirthDecade[1920 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1930] <- "1920-29"
+hrs_data_mex$BirthDecade[1930 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1940] <- "1930-39"
+hrs_data_mex$BirthDecade[1940 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1950] <- "1940-49"
+hrs_data_mex$BirthDecade[1950 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1960] <- "1950-59"
+hrs_data_mex$BirthDecade[1960 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1970] <- "1960-69"
+hrs_data_mex$BirthDecade[1970 <= hrs_data_mex$BirthYear & hrs_data_mex$BirthYear < 1980] <- "1970-79"
+hrs_data_mex$BirthDecade[1980 <= hrs_data_mex$BirthYear] <- "1980+"
+
+# Convert to factors and re-order them
+hrs_data_mex$BirthDecade <- as.factor(hrs_data_mex$BirthDecade)
+print(levels(hrs_data_mex$BirthDecade))
+
+
+# Look at how many people are in each decade
+hrs_data_mex %>% group_by(BirthDecade) %>% summarise(no_rows = length(BirthDecade))
+# Hm. 3 in 1970-80, and 1 in 1980+
+
+# Define a mean function for the bootstrap
+boot_mean <- function(orig, resample) {
+  mean(orig[resample])
+}
+
+mean_bs <- list()
+for (bd in unique(hrs_data_mex$BirthDecade)){
+  print(bd)
+  hrs_sub <- subset(hrs_data_mex, BirthDecade==bd)
+  mean_bs[[bd]] <- boot(hrs_sub$ADMIX3, boot_mean, R = 1000)
+}
+
+rm(df_mean_bs)
+rm(temp_dat)
+rm(temp_dec)
+# convert to long format
+for (bd in names(mean_bs)){
+  temp_dat <- mean_bs[[bd]]$t
+
+  temp_df <- data.frame(temp_dat)
+  temp_df$BirthDecade <- as.factor(bd)
+  names(temp_df) <- c("ADMIX3","BirthDecade")
+  
+  if(exists("df_mean_bs") == F){
+    df_mean_bs <- temp_df
+  } else{
+    df_mean_bs <-rbind(df_mean_bs, temp_df)
+  }
+}
+
+# Re-order the decades from oldest to most recent
+levels(df_mean_bs$BirthDecade)
+df_mean_bs$BirthDecade <- factor(df_mean_bs$BirthDecade, levels(df_mean_bs$BirthDecade)[c(6:1,7,8)])
+
+# Set up the ridgeline plot
+ggplot(df_mean_bs, aes(x = ADMIX3, y = BirthDecade, group = BirthDecade)) +
+  geom_density_ridges()
+
+# Work within the self-identified white population
+hrs_data_mex_white_pre70s <- subset(hrs_data_mex, BirthYear < 1970 && as.character(Race_HRS) == "White")
+lm_admix_mex_white_pre70s <- lm(ADMIX3 ~ BirthYear, data = hrs_data_mex_white_pre70s)
+summary(lm_admix_mex_white_pre70s)
